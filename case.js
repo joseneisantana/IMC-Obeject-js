6ebeefba18ea1a2fdc57bdd0eac2cec590b1eb88
joseneisantana <joseneycosta@hotmail.com>
2025-01-08 17:37:00 -0300

    Finalizando objetos, switch, dicionario

---
+++ case.js
@@ -0,0 +1,50 @@
+/*const  bebidaEscolhida = "fanta";
+
+switch(bebidaEscolhida){
+    case 'coca-cola':
+        console.log('manda coca-cola')
+        break;
+
+        case 'pepsi':
+            console.log('manda pepsi')
+            break;
+
+            case 'fanta':
+                console.log('manda esgoto')
+                break;
+
+                default:
+                    console.log('agua')
+                    break;
+}
+
+*/
+
+const cafe = "sabado"
+
+switch(cafe){
+
+    case 'segunda':
+         console.log ("dia de expresso")
+    case 'terça':
+        console.log("dia  caputino")
+        break  
+    case 'quarta':
+       console.log("dia  de mooca")
+     break  
+    case 'quinta':
+           
+    console.log("dia de cafe com leite")
+    break
+
+    case'sabado':
+    case'domingo':
+    console.log('dia de cafe com torta gelada')
+        
+    default:
+                console.log('agua')
+                break;
+
+    
+
+}
